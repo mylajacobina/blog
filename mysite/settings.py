7290b1a086ba72edc95759de567d9f9388253a51
Jamyla de Morais <jamylademorais@gmail.com>
2020-01-14 16:51:44 -0300

    Minha primeira pagina

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hzjuoe_eh5%ph2!ymg1t&&jamj8ltlcv=97jj%cxhsu)sas)2$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'jamylajacobina.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
